bacd378a7a8f9c19f3f2caff39d2d19d46bd7c54
eduardo.silva <eduardo.silva@compwire.com.br>
2022-09-26 17:13:25 -0300

    Automatizando ainda mais o programa principal

--- calculos/moeda.py
+++ calculos/moeda.py
@@ -16,5 +16,16 @@ def reduzir(p, taxa, format=False):
     c = p - b
     return c if format is False else moeda(c)
 
-def moeda(p=0, moeda= 'R$'):
+def moeda(p, moeda= 'R$'):
     return f'{moeda}{p}'.replace('.', ',')
+
+def resumo(p):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: {moeda(p)}')
+    print(f'Dobro do preço: {dobro(p, True)}')
+    print(f'Metade do preço: {metade(p, True)}')
+    print(f'Aumento de 10%: {aumentar(p, 10, True)}')
+    print(f'Metade do preço: {reduzir(p, 13, True)}')
+    print('-' * 30)
--- teste.py
+++ teste.py
@@ -1,7 +1,4 @@
 from calculos import moeda
 
 p = float(input('Digite o preço: R$ '))
-print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}')
-print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
-print(f'Aumentando 10% de {moeda.moeda(p)}, temos {moeda.aumentar(p, 10, True)}')
-print(f'Reduzindo 13% de {moeda.moeda(p)}, temos {moeda.reduzir(p, 13, True)}. ')
+moeda.resumo(p)
